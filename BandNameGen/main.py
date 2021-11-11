e2734a845af347724e3372c643806424212f34be
Hernan <kermit_cedillo@yahoo.com>
2021-11-10 23:45:14 -0700

    small project

---
+++ BandNameGen/main.py
@@ -0,0 +1,5 @@
+city_name = str(input("Whats the name of the city you grew up on?\n"))
+pet_name = str(input("Whats the name of your pet?\n"))
+
+
+print("The name of the city is " + city_name + " and the name of your pet is " + pet_name)
