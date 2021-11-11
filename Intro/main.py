fc228965f7e4938b5dab8dadefe3adfd1b3c020c
Hernan Cedillo <kermit_cedillo@yahoo.com>
2021-11-10 18:29:31 -0700

    My first print on python

---
+++ Intro/main.py
@@ -0,0 +1,6 @@
+#First program on python
+print("Day 1 - Python Print Function\n")
+print("The function is declared like this:\n")
+#You can also use a single code to print on a string
+print('print("what to print")\n')
+
