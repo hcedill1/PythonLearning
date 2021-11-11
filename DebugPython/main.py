4d6b52130cb4f48e78bf752e22883d22bd5dd794
Hernan Cedillo <kermit_cedillo@yahoo.com>
2021-11-10 18:49:38 -0700

    debugg code

---
+++ DebugPython/main.py
@@ -0,0 +1,5 @@
+print("Day 1 - String Manipulation\n")
+print('String Concatination is done with the " + " sign.\n')
+print('e.g. print("Hello " + "world")\n')
+print("New lines can be created with a backlash and n.")
+
