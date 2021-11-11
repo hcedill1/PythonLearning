01174a08e961d75749e4ca3a21b9dc7117c30e62
Hernan <kermit_cedillo@yahoo.com>
2021-11-10 21:48:21 -0700

    switch input values from the user

--- InputFunction/main.py
+++ InputFunction/main.py
@@ -3,4 +3,19 @@
 #input("What is your name")
 
 #concatinated input from print function
-print("Hello " + input("What is your name?") + "!")
+#print("Hello " + input("What is your name?") + "!")
+
+#name = input("What is your name?")
+
+#print(len(name))
+
+#Coding exercise 
+#Write a program that switches the values stored in the variables a and b 
+a = input("a:")
+b = input("b:")
+
+temp = a
+a = b
+b = temp
+print("a = ",a)
+print("b = ",b)
