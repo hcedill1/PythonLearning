79a51f4b71d3f344c06fbc73f64aedecc4833f97
Hernan Cedillo <kermit_cedillo@yahoo.com>
2021-11-10 19:05:01 -0700

    input function

---
+++ InputFunction/main.py
@@ -0,0 +1,6 @@
+#So whatever the user answers the input will turn into it 
+
+#input("What is your name")
+
+#concatinated input from print function
+print("Hello " + input("What is your name?") + "!")
