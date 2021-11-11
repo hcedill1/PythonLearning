367cf3298131cd8dd76c2b305a4711c3a423ec55
Hernan Cedillo <kermit_cedillo@yahoo.com>
2021-11-10 18:35:22 -0700

    Adding instructions about abou the inditation error

--- Intro/main.py
+++ Intro/main.py
@@ -3,4 +3,9 @@ print("Day 1 - Python Print Function\n")
 print("The function is declared like this:\n")
 #You can also use a single code to print on a string
 print('print("what to print")\n')
+#Inditation error is when you have a space in front of a method or any object 
+#we are concatinating on the print section
+print("Hello " + "Hernan")
+
+
 
